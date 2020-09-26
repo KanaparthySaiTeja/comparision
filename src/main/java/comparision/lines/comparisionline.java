b10fd1f5d3d3a4a4201f7af80c93b53b78363520
saiteja-code <71621072+saiteja-code@users.noreply.github.com>
2020-09-26 08:34:28 +0530

    Update comparisionline.java

--- src/main/java/comparision/lines/comparisionline.java
+++ src/main/java/comparision/lines/comparisionline.java
@@ -13,7 +13,7 @@ public class comparisionline
 
 	    	int x1,x2,y1,y2;
 			int  l1,l2;
-			x1=1; y1=1;
+			x1=2; y1=2;
 			x2=4;y2=4;
 			l1=(int)Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 			System.out.println(l1);
